d1da6ede5f410e006321ce6bf4a12ea6687b94a3
Akhil14324 <akhilvallabhani@gmail.com>
2026-07-22 11:47:02 +0530

    i guess bro

--- softshape-print-agent/edge-server/socketSync.ts
+++ softshape-print-agent/edge-server/socketSync.ts
@@ -72,7 +72,7 @@ export function startSocketSync(): void {
     socket!.emit("edge:register", {
       restaurantId,
       sessionToken: token,
-      edgeVersion: "22.1.0",
+      edgeVersion: "22.2.0",
     });
   });
 
@@ -98,7 +98,7 @@ export function startSocketSync(): void {
     socket!.emit("edge:register", {
       restaurantId,
       sessionToken: token,
-      edgeVersion: "22.1.0",
+      edgeVersion: "22.2.0",
     });
   });
 
